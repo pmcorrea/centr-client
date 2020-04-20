e7beebae47f3fb4179f9225ef34827668011b866
pmcorrea <pmcorrea@me.com>
2020-04-20 19:25:14 -0400

    Updaqted deployment URL in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Create global variables
 export default {
-  API_ENDPOINT: 'https://afternoon-dawn-43994.herokuapp.com',
+  API_ENDPOINT: 'https://vast-beach-19823.herokuapp.com',
   // API_ENDPOINT: 'http://localhost:5000',
   CLIENT_ENDPOINT: 'https://pmcorrea-centr.now.sh/',
   // CLIENT_ENDPOINT: 'http://localhost:3000/',
