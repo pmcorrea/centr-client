7ee8f8aaefa1b605d64d0bf20c485dd6fdaae4b9
pmcorrea <pmcorrea@me.com>
2020-05-02 21:48:18 -0400

    Adjusted config for deployment.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Create global variables
 export default {
-  // API_ENDPOINT: "https://vast-beach-19823.herokuapp.com",
-  API_ENDPOINT: "http://localhost:5000",
-  // CLIENT_ENDPOINT: "https://pmcorrea-centr.now.sh/",
-  CLIENT_ENDPOINT: "http://localhost:3000/",
+  API_ENDPOINT: "https://vast-beach-19823.herokuapp.com",
+  // API_ENDPOINT: "http://localhost:5000",
+  CLIENT_ENDPOINT: "https://pmcorrea-centr.now.sh/",
+  // CLIENT_ENDPOINT: "http://localhost:3000/",
   TOKEN_KEY: "centr-client-auth-key",
 };
